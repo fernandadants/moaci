3a388ed49316e11bb4b82dc4cdd3a193e2b4fed7
Fernanda Azevedo <fernandadants@hotmail.com>
2023-10-18 17:51:58 -0300

    ligar ou desligar de acordo com tempo

--- include/header.h
+++ include/header.h
@@ -4,6 +4,10 @@
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
 
+#define LED 33
+#define BUZZER 35
+#define BUTTOM 27
+
 const char* apSSID = "MoaciNet"; // Nome do Access Point
 const char* apPassword = "moaci1234"; // Senha do Access Point
 
@@ -15,3 +19,51 @@ struct Pessoa {
 const int maxPessoas = 10; // Número máximo de pessoas a serem armazenadas
 Pessoa pessoas[maxPessoas]; // Array de structs Pessoa
 int numPessoas = 0; // Contador de pessoas armazenadas
+
+int primeiro_i = 0; // Índice do primeiro elemento na fila
+int ultimo_i = 0; // Índice do último elemento na fila
+
+bool led_aceso = false;
+unsigned long ultimo_tempo = 0;
+unsigned long proxima_chamada = 0;
+unsigned long tempo_limite = 1 * 60000;
+unsigned long tempo_botao = 0;
+
+bool estado_botao = false;
+
+//Adiciona a pessoa no fim da lista
+void enqueue(const Pessoa &p) {
+  if (numPessoas < maxPessoas) {
+    pessoas[ultimo_i] = p;
+    ultimo_i = (ultimo_i + 1) % maxPessoas; // Circular, para reutilizar espaço
+    numPessoas++;
+  }
+}
+
+//Retorna a pessoa mais antiga da lista
+Pessoa dequeue() {
+  if (numPessoas > 0) {
+    Pessoa p = pessoas[primeiro_i];
+    primeiro_i = (primeiro_i + 1) % maxPessoas; // Circular, para reutilizar espaço
+    numPessoas--;
+    return p;
+  }
+  // Retorne uma pessoa vazia
+  Pessoa vazia;
+  vazia.nome = "";
+  vazia.tempo = 0;
+  return vazia;
+}
+
+String listaPessoas() {
+  String lista = "<tbody>";
+  for (int i = 0; i < numPessoas; i++) {
+    lista += "<tr>";
+    lista += "<td>" + String(i) + "</td>";
+    lista += "<td>" + pessoas[i].nome + "</td>";
+    lista += "<td>0" + String(pessoas[i].tempo) + ":00</td>";
+    lista += "/<tr>";
+  }
+  lista += "</tbody>";
+  return lista;
+}
--- src/moaci_teste.ino
+++ src/moaci_teste.ino
@@ -2,41 +2,26 @@
 
 AsyncWebServer server(80);
 
-/*const char* html = R"rawliteral(
-<!DOCTYPE HTML><html><head><title>Cadastro de Pessoas</title></head>
-<body>
-  <h2>Cadastro de Pessoas</h2>
-  <form action="/cadastro" method="post">
-    Nome: <input type="text" name="nome"><br>
-    Tempo: <input type="number" name="tempo"><br>
-    <input type="submit" value="Cadastrar">
-  </form>
-  <h2>Pessoas Cadastradas</h2>
-  %pessoas%
-</body></html>)rawliteral";*/
-
 void notFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", "Not found");
 }
 
-String listaPessoas() {
-  String lista = "<tbody>";
-  for (int i = 0; i < numPessoas; i++) {
-    lista += "<tr>";
-    lista += "<td>" + String(i) + "</td>";
-    lista += "<td>" + pessoas[i].nome + "</td>";
-    lista += "<td>0" + String(pessoas[i].tempo) + ":00</td>";
-    lista += "/<tr>";
-  }
-  lista += "</tbody>";
-  return lista;
-}
-
 void setup() {
+  //Definindo portas de cada item
+  pinMode(LED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  pinMode(BUTTOM, INPUT_PULLUP);
+
+  //Começando com led apagado
+  digitalWrite(LED, LOW);
+
+  //Inicia a comunicação com o serial
   Serial.begin(115200);
   
   // Configura o ESP32 como um ponto de acesso (Access Point)
   WiFi.mode(WIFI_AP);
+
+  // Define o nome e senha da rede
   WiFi.softAP(apSSID, apPassword);
   Serial.println("Access Point criado. SSID: " + String(apSSID));
 
@@ -46,35 +31,30 @@ void setup() {
     return;
   }
 
-  //Rodando site
-  /*server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    String response = html;
-    response.replace("%pessoas%", listaPessoas());
-    request->send(200, "text/html", response);
-  });*/
-
+  // Definindo rotas para o site
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     String response = SPIFFS.open("/index.html", "r").readString();
     response.replace("<%pessoas%>", listaPessoas());
     request->send(200, "text/html", response);
   });
 
-  // Route to load style.css file
+  // Rota para o arquivo css
   server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SPIFFS, "/style.css", "text/css");
   });
 
-
+  // Rota para o cadastro de novas pessoas
   server.on("/cadastro", HTTP_POST, [](AsyncWebServerRequest *request){
     String nome = request->arg("nome");
     int tempo = request->arg("tempo").toInt();
 
     // Verifica se o array de pessoas ainda tem espaço
     if (numPessoas < maxPessoas) {
-      // Armazena os dados da pessoa no array
-      pessoas[numPessoas].nome = nome;
-      pessoas[numPessoas].tempo = tempo;
-      numPessoas++;
+      //Adicionando as novas pessoas ao final da fila
+      Pessoa p;
+      p.nome = nome;
+      p.tempo = tempo;
+      enqueue(p);
     }
 
     // Redireciona de volta para a página principal
@@ -87,8 +67,28 @@ void setup() {
 }
 
 void loop() {
-  // Obtenha e mostre o endereço IP do Access Point continuamente no loop
-  //Serial.print("Endereço IP do Access Point: ");
-  //Serial.println(WiFi.softAPIP());
-  //delay(10000); // Atraso de 10 segundos entre as exibições
+  //Definindo tempo de carregamento do esp
+  unsigned long currentTime = millis();
+
+  // Verificar se o LED deve ser ligado
+  if (numPessoas > 0 && !led_aceso) {
+    Pessoa proximaPessoa = pessoas[0];
+    if (currentTime - ultimo_tempo >= proximaPessoa.tempo * 60000) {
+      digitalWrite(LED, HIGH); // Ligar o LED 
+      led_aceso = true; // Definir o estado do LED como ligado
+      // Marcar o tempo em que o LED ligou
+      ultimo_tempo = currentTime;
+      // Calcular o tempo para chamar a próxima pessoa
+      proxima_chamada = currentTime + proximaPessoa.tempo * 60000;
+    }
+  }
+
+  // Verificando se é hora de chamar a próxima pessoa
+  if (numPessoas > 0 && currentTime >= proxima_chamada) {
+    Pessoa chamada = dequeue();
+    ultimo_tempo = currentTime; // Marcar o tempo em que o LED ligou
+    digitalWrite(LED, LOW); // Desligar o LED
+    led_aceso = false; // Definir o estado do LED como desligado
+    Serial.println("Chamada: " + chamada.nome);
+  }
 }
